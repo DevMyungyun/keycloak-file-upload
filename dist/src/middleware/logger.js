2599280e1dae73ec71a8bd98b76437e435a808c3
DevMyungyun <myungyuncho82@gmail.com>
2022-08-29 23:24:49 -0500

    git ignore apply

--- dist/src/middleware/logger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const loggerMiddleware = (req, resp, next) => {
-    console.log((new Date).toString(), 'Request Accepted:', req.method, req.path);
-    next();
-};
-exports.default = loggerMiddleware;
-//# sourceMappingURL=logger.js.map
--- dist/src/service/uploadAbstractClass.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class upload {
-}
-exports.default = upload;
-//# sourceMappingURL=uploadAbstractClass.js.map
